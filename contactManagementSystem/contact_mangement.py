96cb4c8b1b0b7cacee221bd7cde7f362ed607fd1
Abu Hanif <ahanif2500@gmail.com>
2024-12-03 02:55:17 +0600

    Search feature implemented

--- contactManagementSystem/contact_mangement.py
+++ contactManagementSystem/contact_mangement.py
@@ -5,7 +5,8 @@ while run:
     print("Choose Menu")
     print("1. Add Contact")
     print("2. View Contacts")
-    print("3. Exit")
+    print("3. Search Contacts")
+    print("4. Exit")
 
     choice = int(input("Enter your choice: "))
     if choice == 1:
@@ -32,5 +33,40 @@ while run:
                 name, email, phone, address = c.split(",")
                 print(f"{name} | {email} | {phone} |{address}", end='')
     if choice == 3:
+        search_by = input("Find by (name|email|phone|address): ")
+        if search_by.lower() not in ('name', 'email', 'phone', 'address'):
+            raise Exception("Incorrect Keyword to Search")
+        search = input(f"Enter a {search_by} to search: ").lower()
+        search_results = []
+        with open('contacts.csv', 'r') as f:
+            contacts = f.readlines()
+            for c in contacts:
+                name, email, phone, address = c.split(",")
+                contact = Contact(name, email, phone, address)
+                if search_by.lower() == "name":
+                    if search == name.lower():
+                        search_results.append(contact)
+                    else:
+                        raise Exception("Contact Name Not Found")
+                elif search_by.lower() == 'email':
+                    if search == email.lower():
+                        search_results.append(Contact(name, email, phone, address))
+                    else:
+                        raise Exception("Contact Email Not Found")
+                elif search_by.lower() == 'phone':
+                    if search == phone.lower():
+                        search_results.append(Contact(name, email, phone, address))
+                    else:
+                        raise Exception("Contact Phone Number Not Found")
+                else:
+                    if search == address.lower():
+                        search_results.append(Contact(name, email, phone, address))
+                    else:
+                        raise Exception("Contact Address Not Found")
+
+            for r in search_results:
+                print(r)
+
+    if choice == 4:
         run = False
         print("Thank You!")
