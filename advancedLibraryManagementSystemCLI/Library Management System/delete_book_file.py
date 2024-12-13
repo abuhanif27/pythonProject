4e3e7e74a176c994dbff45bb938e37db5f9105ed
Abu Hanif <ahanif2500@gmail.com>
2024-12-13 20:42:49 +0600

    modify delete_books.py

--- advancedLibraryManagementSystemCLI/Library Management System/delete_book_file.py
+++ advancedLibraryManagementSystemCLI/Library Management System/delete_book_file.py
@@ -1,12 +1,13 @@
-import save_all_books
+from save_all_books import save_all_books
+
 
 def delete_books(all_books):
-    search_book = input("Enter Book Title to Delete: ")
+    search_term = input("Enter Book Title or ISBN to Delete: ")
     for book in all_books:
-        if book["title"] == search_book:
+        if book["title"] == search_term or str(book["isbn"]) == search_term:
             all_books.remove(book)
-            save_all_books.save_all_books(all_books)
-            print("Book Deleted Successfully")
-            return all_books
-    
-    print("Book Not Found")
+            save_all_books(all_books)
+            print("Book Deleted Successfully!")
+            return
+
+    print("Book Not Found.")
