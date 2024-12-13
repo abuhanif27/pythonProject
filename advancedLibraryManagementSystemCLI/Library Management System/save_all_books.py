4026db75d2575424716a735d7ffcd712038c3cbc
Abu Hanif <ahanif2500@gmail.com>
2024-12-13 20:39:06 +0600

    save_all_books.py

--- advancedLibraryManagementSystemCLI/Library Management System/save_all_books.py
+++ advancedLibraryManagementSystemCLI/Library Management System/save_all_books.py
@@ -1,9 +1,3 @@
-# def save_all_books(all_books):
-#     with open("all_books.csv", "w") as fp:
-#         for book in all_books:
-#             line = f"{book['title']}, {book['author']}, {book['isbn']}, {book['year']}, {book['price']}, {book['quantity']}\n"
-#             fp.write(line)
-
 import json
 
 def save_all_books(all_books):
